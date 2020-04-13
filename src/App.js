bd71964e1a5d4ba75740638b3159f74e95b25528
Paradorn Pimporn <paraz@Paradorns-MacBook-Pro.local>
2020-04-13 15:37:15 +0700

    Creating the header

--- src/App.js
+++ src/App.js
@@ -1,58 +1,43 @@
 import React from "react";
 import "./App.css";
+import SideNavMe from "./Sidenav";
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <h5 className="center-align">Center aliment</h5>
-        <h5 className="left-align">Left aliment</h5>
-        <h5 className="right-align">Right aliment</h5>
-      </div>
+      <header>
+        <nav className="nav-wrapper transparent">
+          <div className="container">
+            <SideNavMe></SideNavMe>
+            <a href="#" className="brand-logo">
+              Photo Ninja
+            </a>
+            <ul className="right hide-on-med-and-down">
+              <li>
+                <a href="#">Photo's</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
 
-      <div className="container grey lighten-2 box valign-wrapper">
-        <h5>Vertical aliment</h5>
-      </div>
-
-      <div className="container">
-        <p className="truncate">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Dapibus
-          ultrices in iaculis nunc sed augue lacus. Quam nulla porttitor massa
-          id neque aliquam. Ultrices mi tempus imperdiet nulla malesuada. Eros
-          in cursus turpis massa tincidunt dui ut ornare lectus. Egestas sed sed
-          risus pretium. Lorem dolor sed viverra ipsum. Gravida rutrum quisque
-          non tellus. Rutrum tellus pellentesque eu tincidunt tortor. Sed
-          blandit libero volutpat sed cras ornare. Et netus et malesuada fames
-          ac. Ultrices eros in cursus turpis massa tincidunt dui ut ornare.
-          Lacus sed viverra tellus in. Sollicitudin ac orci phasellus egestas.
-          Purus in mollis nunc sed. Sollicitudin ac orci phasellus egestas
-          tellus rutrum tellus pellentesque. Interdum consectetur libero id
-          faucibus nisl tincidunt eget.
-        </p>
-      </div>
-
-      <div className="container">
-        <h4>Button</h4>
-        <a href="#" className="btn wave-effect wave-dark">
-          Normal Button
-        </a>
-        <a href="#" className="btn indigo wave-effect wave-light">
-          Indigo Button
-        </a>
-        <a href="#" className="btn disabled">
-          Disable Button
-        </a>
-      </div>
-
-      <div className="container">
-        <h4>Icon</h4>
-        <i className="material-icons red-text">error</i>
-        <a href="#" className="btn indigo">
-          <span>Send</span>
-          <i className="material-icons right">send</i>
-        </a>
-      </div>
+            <ul className="sidenav grey lighten-2" id="mobile-menu">
+              <li>
+                <a href="#">Photo's</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
     </div>
   );
 }
---
+++ src/Sidenav.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { SideNavItem, SideNav } from "react-materialize";
+
+const SideNavMe = () => {
+  return (
+    <div className="container">
+      <style>
+        {`
+            #root > div > div {
+              z-index: 99999 !important;
+            }
+          `}
+      </style>
+      <SideNav
+        id="SideNav-10"
+        options={{
+          draggable: true,
+        }}
+        trigger={
+          <a href="#" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+          </a>
+        }
+      >
+        <SideNavItem
+          user={{
+            background: "https://placeimg.com/640/480/tech",
+            email: "jdandturk@gmail.com",
+            // image: "./img/man.jpg",
+            name: "John Doe",
+          }}
+          userView
+        />
+        <SideNavItem>Photo's</SideNavItem>
+        <SideNavItem href="#!second">Services</SideNavItem>
+        <SideNavItem divider />
+        <SideNavItem subheader>Page's Info</SideNavItem>
+        <SideNavItem href="#!third" waves>
+          Contact
+        </SideNavItem>
+      </SideNav>
+    </div>
+  );
+};
+export default SideNavMe;
