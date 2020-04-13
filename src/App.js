62c85022a4b570c95958b6fe00e991d9678256fb
Paradorn Pimporn <paraz@Paradorns-MacBook-Pro.local>
2020-04-13 11:01:32 +0700

    5:Colours

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="container">
+        <h5 className="center-align">Center aliment</h5>
+        <h5 className="left-align">Left aliment</h5>
+        <h5 className="right-align">Right aliment</h5>
+      </div>
+
+      <div className="container grey lighten-2 box valign-wrapper">
+        <h5>Vertical aliment</h5>
+      </div>
+
+      <div className="container">
+        <p className="truncate">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Dapibus
+          ultrices in iaculis nunc sed augue lacus. Quam nulla porttitor massa
+          id neque aliquam. Ultrices mi tempus imperdiet nulla malesuada. Eros
+          in cursus turpis massa tincidunt dui ut ornare lectus. Egestas sed sed
+          risus pretium. Lorem dolor sed viverra ipsum. Gravida rutrum quisque
+          non tellus. Rutrum tellus pellentesque eu tincidunt tortor. Sed
+          blandit libero volutpat sed cras ornare. Et netus et malesuada fames
+          ac. Ultrices eros in cursus turpis massa tincidunt dui ut ornare.
+          Lacus sed viverra tellus in. Sollicitudin ac orci phasellus egestas.
+          Purus in mollis nunc sed. Sollicitudin ac orci phasellus egestas
+          tellus rutrum tellus pellentesque. Interdum consectetur libero id
+          faucibus nisl tincidunt eget.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
